85cd5db3e7844197ba1d17a117227889976943b9
julielee16 <juhoonlee@kaist.ac.kr>
2020-11-26 04:07:01 +0900

    Fixing flag

--- minchoom/src/components/Home.js
+++ minchoom/src/components/Home.js
@@ -227,14 +227,11 @@ class Home extends Component {
                         <Fab 
                             variant="extended" 
                             style={{top: '-45px', fontWeight: '600', fontSize: '1.4em'}} 
-                            
                         >
                         🚩Flag
                         </Fab>
                         
-                        <Button.Group 
-                            vertical
-                        >
+                        <Button.Group vertical>
                             <Button onClick={() => addFlag('Activity')}><Image src={Activity} avatar/> Activity</Button>
                             <Button onClick={() => addFlag('Emphasis')}><Image src={Emphasis} avatar/> Emphasis</Button>
                             <Button onClick={() => addFlag('Exclusive')}><Image src={Exclusive} avatar/> Exclusive Material</Button>
