00a4c48f39d85ea5d33d127d055208d239a038c5
julielee16 <juhoonlee@kaist.ac.kr>
2020-11-26 01:12:01 +0900

    Changing catchup

--- minchoom/src/components/Catchup.js
+++ minchoom/src/components/Catchup.js
@@ -6,6 +6,7 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const databaseURL = "https://minchoom-cs473.firebaseio.com"
+const db = ['/questions', '/answers']
 
 export default class Catchup extends React.Component {
   constructor(props) {
@@ -14,11 +15,11 @@ export default class Catchup extends React.Component {
       questions: [],
       answers: [],
       formValue: '',
-      sessionId: 'julie'
-
+      sessionId: 'julie',
+      asking: true
     }
-    this.sendData = this.sendData.bind(this);
     this.getData = this.getData.bind(this);
+    this.sendData = this.sendData.bind(this);
     this.sendQuestion = this.sendQuestion.bind(this);
   }
 
@@ -32,8 +33,8 @@ export default class Catchup extends React.Component {
     //   );
   }
 
-  sendData = (dataDict) => {
-    return fetch( `${databaseURL+'/catchup'}/.json`, {
+  sendData = (dataDict, index) => {
+    return fetch( `${databaseURL+'/catchup'+ db[index]}/.json`, {
       method: 'POST',
       body: JSON.stringify(dataDict)
     }).then(res => {
@@ -42,7 +43,7 @@ export default class Catchup extends React.Component {
       }
       return res.json();
     }).then(() => {
-      console.log("CatchUp succesfully sent!")
+      console.log("CatchUp Question succesfully sent!")
     })
   }
 
@@ -53,12 +54,14 @@ export default class Catchup extends React.Component {
         }
         return res.json();
     }).then(res => {
+        const q = 'questions'
+        const a = 'answers'
         const question_keys = Object.keys(res['questions'])
         const answer_keys = Object.keys(res['answers'])
-        const questions = question_keys.map((k)=>[res['questions'][k]['answered'], res['questions'][k]['answeredSessions'], res['questions'][k]['flagId'], res['questions'][k]['questionId'], res['questions'][k]['questionText'], res['questions'][k]['sessionId'], res['questions'][k]['time']]).sort(function(first, second) {
+        const questions = question_keys.map((k)=>[res[q][k]['answered'], res[q][k]['answeredSessions'], res[q][k]['flagId'], res[q][k]['flagLabel'], res[q][k]['questionText'], res[q][k]['sessionId'], res[q][k]['time']]).sort(function(first, second) {
             return second[6] - first[6];
           })
-        const answers = answer_keys.map((k)=>[res['answers'][k]['answerId'], res['answers'][k]['answerText'], res['answers'][k]['flagId'], res['answers'][k]['liked'], res['answers'][k]['questionId'], res['answers'][k]['sessionId'], res['answers'][k]['time'], res['answers'][k]['upvotes']]).sort(function(first, second) {
+        const answers = answer_keys.map((k)=>[res[a][k]['answerText'], res[a][k]['flagId'], res[a][k]['flagLabel'], res[a][k]['liked'], res[a][k]['questionId'], res[a][k]['sessionId'], res[a][k]['time'], res[a][k]['upvotes']]).sort(function(first, second) {
           return second[6] - first[6];
         })
         this.setState({
@@ -74,39 +77,67 @@ export default class Catchup extends React.Component {
     this.sendData(
         {
           answered: false,
-          answeredSessions: '',
-          flagId: 1,
-          questionId: 1,
+          answeredSessions: [],
+          flagId: this.props.flagId,
+          flagLabel: this.props.flagLabel,
           questionText: this.state.formValue,
           sessionId: this.state.sessionId,
           //sessionId: "pat",
-          time: new Date()
-        }
+          time: this.props.time,
+        }, 0
+    )
+    this.setState({
+      formValue: '',
+      asking: true
+    })
+  }
+
+  sendAnswer = async (e) => {
+    e.preventDefault();
+
+    this.sendData(
+        {
+          answerText: false,
+          flagId: this.props.flagId,
+          flagLabel: this.props.flagLabel,
+          liked:false,
+          questionId: '',
+          sessionId: this.state.sessionId,
+          //sessionId: "pat",
+          time: new Date(),
+          upvotes: 0
+        }, 1
     )
     this.setState({
       formValue: ''
-  })
+    })
     //dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  handleQuestion = () => {
+      this.setState({
+        asking: false
+      })
   }
 
 
   render() {
     const { } = this.props;
-    const {questions, answers, formValue, sessionId} = this.state;
-    const { sendQuestion } = this;
+    const {questions, answers, formValue, sessionId, asking} = this.state;
+    const { sendQuestion, handleQuestion, sendAnswer } = this;
     return (
             <chat>
               <main>
                 <div className="type">
                     Notice    
                 </div>
+                {/* This is where the screenshot image goes */}
+                <img className='questionImg' src={lecture} />
                 <div>{questions 
                 ?
                 questions.map(q => { return(
                 <>
-                <div className='question'>
-                {/* This is where the screenshot image goes */}
-                  <img className='questionImg' src={lecture} />
+                <div className='question'onClick={handleQuestion}>
                   <div className="q">Q. {q[4]}</div>
                 </div></>)})
                 :
@@ -117,7 +148,7 @@ export default class Catchup extends React.Component {
                 answers.map(a => { return(
                 <>
                 <div>
-                  <div className="a">A. {a[1]}</div>
+                  <div className="a">A. {a[0]}</div>
                 </div></>)})
                 :
                 <div>no answers!</div>  
@@ -128,13 +159,13 @@ export default class Catchup extends React.Component {
                 {/* <span ref={dummy}></span> */}
               </main>
           
-              <form onSubmit={sendQuestion}>
+              <form onSubmit={asking ? sendQuestion: sendAnswer}>
                   
                   <textarea value={formValue} onChange={(e) => this.setState({formValue: e.target.value})} placeholder="Ask your question!" />
             
                   <button type="submit" disabled={!formValue}>🕊️</button>
             
-                </form>
+              </form>
           </chat>)
           }
 };
