479bc6b7a69c2502942ce9b17e604d8640e45aee
jeongeonp <jeongeonpark1@gmail.com>
2020-11-26 04:06:40 +0900

    Refresh DB

--- minchoom/src/components/Home.js
+++ minchoom/src/components/Home.js
@@ -237,7 +237,7 @@ class Home extends Component {
                         >
                             <Button onClick={() => addFlag('Activity')}><Image src={Activity} avatar/> Activity</Button>
                             <Button onClick={() => addFlag('Emphasis')}><Image src={Emphasis} avatar/> Emphasis</Button>
-                            <Button onClick={() => addFlag('Exclusive')}><Image src={Exclusive} avatar/> Exclusive Material</Button>
+                            <Button onClick={() => addFlag('Exclusive Material')}><Image src={Exclusive} avatar/> Exclusive Material</Button>
                             <Button onClick={() => addFlag('Notice')}><Image src={Notice} avatar/> Notice</Button>
                             <Button onClick={() => addFlag('Q&A')}><Image src={QnA} avatar/> Q&A</Button>
                         </Button.Group>
